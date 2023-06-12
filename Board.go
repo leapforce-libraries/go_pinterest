a8d485159cf5db707b3f6f2cec3612db0aa622ae
caselongo <caselongo@gmail.com>
2023-06-12 17:49:48 +0200

    added Board.go, Pin.go

---
+++ Board.go
@@ -0,0 +1,86 @@
+package pinterest
+
+import (
+	"fmt"
+	errortools "github.com/leapforce-libraries/go_errortools"
+	go_http "github.com/leapforce-libraries/go_http"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type Board struct {
+	Id                  string     `json:"id"`
+	CreatedAt           time.Time  `json:"created_at"`
+	BoardPinsModifiedAt time.Time  `json:"board_pins_modified_at"`
+	Name                string     `json:"name"`
+	Description         string     `json:"description"`
+	CollaboratorCount   int64      `json:"collaborator_count"`
+	PinCount            int64      `json:"pin_count"`
+	FollowerCount       int64      `json:"follower_count"`
+	Media               BoardMedia `json:"media"`
+	Owner               Owner      `json:"owner"`
+	Privacy             string     `json:"privacy"`
+}
+
+type BoardMedia struct {
+	ImageCoverUrl    string   `json:"image_cover_url"`
+	PinThumbnailUrls []string `json:"pin_thumbnail_urls"`
+}
+
+type Owner struct {
+	Username string `json:"username"`
+}
+
+type ListBoardsConfig struct {
+	AdAccountId *string
+	PageSize    *int64
+	Privacy     *string
+}
+
+type ListBoardsResponse struct {
+	Items    []Board
+	Bookmark string
+}
+
+func (service *Service) ListBoards(cfg *ListBoardsConfig) (*[]Board, *errortools.Error) {
+	var boards []Board
+
+	var values = url.Values{}
+	if cfg != nil {
+		if cfg.AdAccountId != nil {
+			values.Set("ad_account_id", *cfg.AdAccountId)
+		}
+		if cfg.PageSize != nil {
+			values.Set("page_size", fmt.Sprintf("%v", *cfg.PageSize))
+		}
+		if cfg.Privacy != nil {
+			values.Set("privacy", *cfg.Privacy)
+		}
+	}
+
+	for {
+		var response ListBoardsResponse
+
+		requestConfig := go_http.RequestConfig{
+			Method:        http.MethodGet,
+			Url:           service.url(fmt.Sprintf("boards?%s", values.Encode())),
+			ResponseModel: &response,
+		}
+
+		_, _, e := service.httpRequest(&requestConfig)
+		if e != nil {
+			return nil, e
+		}
+
+		boards = append(boards, response.Items...)
+
+		if response.Bookmark == "" {
+			break
+		}
+
+		values.Set("bookmark", response.Bookmark)
+	}
+
+	return &boards, nil
+}
---
+++ Pin.go
@@ -0,0 +1,91 @@
+package pinterest
+
+import (
+	"fmt"
+	errortools "github.com/leapforce-libraries/go_errortools"
+	go_http "github.com/leapforce-libraries/go_http"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
+
+type Pin struct {
+	Id              string    `json:"id"`
+	CreatedAt       time.Time `json:"created_at"`
+	Link            string    `json:"link"`
+	Title           string    `json:"title"`
+	Description     string    `json:"description"`
+	DominantColor   string    `json:"dominant_color"`
+	AltText         string    `json:"alt_text"`
+	CreativeType    string    `json:"creative_type"`
+	BoardId         string    `json:"board_id"`
+	BoardSectionId  string    `json:"board_section_id"`
+	BoardOwner      Owner     `json:"board_owner"`
+	IsOwner         bool      `json:"is_owner"`
+	Media           PinMedia  `json:"media"`
+	ParentPinId     string    `json:"parent_pin_id"`
+	IsStandard      bool      `json:"is_standard"`
+	HasBeenPromoted bool      `json:"has_been_promoted"`
+	Note            string    `json:"note"`
+}
+
+type PinMedia struct {
+	MediaType string `json:"media_type"`
+}
+
+type ListPinsConfig struct {
+	BoardId       string
+	AdAccountId   *string
+	PageSize      *int64
+	CreativeTypes *[]string
+}
+
+type ListPinsResponse struct {
+	Items    []Pin
+	Bookmark string
+}
+
+func (service *Service) ListPins(cfg *ListPinsConfig) (*[]Pin, *errortools.Error) {
+	if cfg == nil {
+		return nil, errortools.ErrorMessage("ListPinsConfig must not be bil")
+	}
+
+	var pins []Pin
+
+	var values = url.Values{}
+	if cfg.AdAccountId != nil {
+		values.Set("ad_account_id", *cfg.AdAccountId)
+	}
+	if cfg.PageSize != nil {
+		values.Set("page_size", fmt.Sprintf("%v", *cfg.PageSize))
+	}
+	if cfg.CreativeTypes != nil {
+		values.Set("creative_types", strings.Join(*cfg.CreativeTypes, ","))
+	}
+
+	for {
+		var response ListPinsResponse
+
+		requestConfig := go_http.RequestConfig{
+			Method:        http.MethodGet,
+			Url:           service.url(fmt.Sprintf("boards/%s/pins?%s", cfg.BoardId, values.Encode())),
+			ResponseModel: &response,
+		}
+
+		_, _, e := service.httpRequest(&requestConfig)
+		if e != nil {
+			return nil, e
+		}
+
+		pins = append(pins, response.Items...)
+
+		if response.Bookmark == "" {
+			break
+		}
+
+		values.Set("bookmark", response.Bookmark)
+	}
+
+	return &pins, nil
+}
